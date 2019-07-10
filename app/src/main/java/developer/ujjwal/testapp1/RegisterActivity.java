e01c6979c5c0eb0403cca93a7ee44dd5bb5faf16
Ujjwal Maity <ujjwalmaity98@gmail.com>
2019-07-11 02:56:02 +0530

    Fetch Secure Android ID

--- app/src/main/java/developer/ujjwal/testapp1/RegisterActivity.java
+++ app/src/main/java/developer/ujjwal/testapp1/RegisterActivity.java
@@ -1,15 +1,17 @@
 package developer.ujjwal.testapp1;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText name, phone, mac;
+    EditText name, phone;
     Button submit;
 
     @Override
@@ -19,15 +21,18 @@ public class RegisterActivity extends AppCompatActivity {
 
         name = findViewById(R.id.activityRegisterName);
         phone = findViewById(R.id.activityRegisterPhone);
-        mac = findViewById(R.id.activityRegisterMac);
         submit = findViewById(R.id.activityRegisterSubmit);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Secure Android ID :: https://medium.com/@ssaurel/how-to-retrieve-an-unique-id-to-identify-android-devices-6f99fd5369eb
+                String android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+                Toast.makeText(getApplicationContext(), android_id, Toast.LENGTH_SHORT).show();
+
                 Intent in = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(in);
-                finish();
+                //startActivity(in);
+                //finish();
             }
         });
     }
