7c8c99ba4f2d1e35069e681a3c09600e4dab7216
Ujjwal Maity <ujjwalmaity98@gmail.com>
2019-07-11 23:28:00 +0530

    Include MAC Address

--- app/src/main/java/developer/ujjwal/testapp1/RegisterActivity.java
+++ app/src/main/java/developer/ujjwal/testapp1/RegisterActivity.java
@@ -1,5 +1,6 @@
 package developer.ujjwal.testapp1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 
@@ -14,6 +15,9 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -62,11 +66,12 @@ public class RegisterActivity extends AppCompatActivity {
                 }
                 phoneNumber = "+91" + phoneNumber;
 
-
+                Toast.makeText(getApplicationContext(), getBeautifulMac(getApplicationContext()), Toast.LENGTH_SHORT).show();
+/*
                 //Secure Android ID :: https://medium.com/@ssaurel/how-to-retrieve-an-unique-id-to-identify-android-devices-6f99fd5369eb
                 String android_id = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
                 Toast.makeText(getApplicationContext(), "Secure Android ID: " + android_id, Toast.LENGTH_SHORT).show();
-
+*/
                 Intent in = new Intent(getApplicationContext(), OtpActivity.class);
                 in.putExtra("phoneNumber", phoneNumber);
                 startActivity(in);
@@ -86,5 +91,40 @@ public class RegisterActivity extends AppCompatActivity {
                 phone.setError("Invalid Phone");
                 break;
         }
+    }
+
+    public static String getBeautifulMac(Context context) {
+        String macAddr = getMacAddress(context);
+        return macAddr.replaceAll(":", "");
+    }
+
+    public static String getMacAddress(Context context) {
+        String macAddr = "";
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().toLowerCase().startsWith("wlan")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    //res1.append(Integer.toHexString(b & 0xFF) + ":");
+                    res1.append(String.format("%02X:", b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString().toLowerCase();
+            }
+        } catch (Exception ex) {
+        }
+        return "";
+
+        //return macAddr;
     }
 }
